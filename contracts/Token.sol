05ea51a2feb423907b9370c4a1f26039d5f77e70
riordant <riordant@tcd.ie>
2021-01-07 19:01:37 +0700

    Add burnable

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,9 +2,10 @@ pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract Token is ERC20, ERC20Detailed, Ownable {
+contract Token is ERC20, ERC20Detailed, ERC20Burnable, Ownable {
 
     string private _name = "DAO Maker Token";
     string private constant _symbol = "DAO";
